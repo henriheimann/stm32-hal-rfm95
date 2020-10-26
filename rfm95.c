975cb166b673853c7ce02145d8cc77e13daeeca7
Henri Heimann <henriheimann@gmail.com>
2020-10-26 21:17:14 +0100

    Initial work on initialisation function

--- rfm95.c
+++ rfm95.c
@@ -1,1 +1,64 @@
 #include "rfm95.h"
+
+#include <assert.h>
+
+#define RFM9x_VER 0x12
+
+/**
+ * Registers addresses.
+ */
+typedef enum
+{
+	RFM95_REGISTER_VERSION = 0x42
+} rfm95_register_t;
+
+bool rfm95_read(rfm95_handle_t *handle, rfm95_register_t reg, uint8_t *buffer)
+{
+	HAL_GPIO_WritePin(handle->nss_port, handle->nss_pin, GPIO_PIN_RESET);
+
+	uint8_t transmit_buffer = (uint8_t)reg & 0x7fu;
+
+	if (HAL_SPI_Transmit(handle->spi_handle, &transmit_buffer, 1, RFM95_SPI_TIMEOUT) != HAL_OK) {
+		return false;
+	}
+
+	if (HAL_SPI_Receive(handle->spi_handle, buffer, 1, RFM95_SPI_TIMEOUT) != HAL_OK) {
+		return false;
+	}
+
+	return true;
+}
+
+bool rfm95_init(rfm95_handle_t *handle)
+{
+	assert(handle->spi_handle->Init.Mode == SPI_MODE_MASTER);
+	assert(handle->spi_handle->Init.Direction == SPI_DIRECTION_2LINES);
+	assert(handle->spi_handle->Init.DataSize == SPI_DATASIZE_8BIT);
+	assert(handle->spi_handle->Init.CLKPolarity == SPI_POLARITY_LOW);
+	assert(handle->spi_handle->Init.CLKPhase == SPI_PHASE_1EDGE);
+
+	// Module must be powered on for at least 10ms before usage
+	uint32_t tick = HAL_GetTick();
+	if (tick < 10) {
+		HAL_Delay(10 - tick);
+	}
+
+	uint8_t version;
+	if (!rfm95_read(handle, RFM95_REGISTER_VERSION, &version)) {
+		return false;
+	}
+
+	if (version != RFM9x_VER) {
+		return false;
+	}
+
+	return true;
+}
+
+void rfm95_reset(rfm95_handle_t *handle)
+{
+	HAL_GPIO_WritePin(handle->nrst_port, handle->nrst_pin, GPIO_PIN_RESET);
+	HAL_Delay(1); // 0.1ms would theoretically be enough
+	HAL_GPIO_WritePin(handle->nrst_port, handle->nrst_pin, GPIO_PIN_SET);
+	HAL_Delay(5);
+}
--- rfm95.h
+++ rfm95.h
@@ -1,1 +1,69 @@
 #pragma once
+
+#include <stdbool.h>
+
+#if (defined STM32L011xx) || (defined STM32L021xx) || \
+	(defined STM32L031xx) || (defined STM32L041xx) || \
+	(defined STM32L051xx) || (defined STM32L052xx) || (defined STM32L053xx) || \
+	(defined STM32L061xx) || (defined STM32L062xx) || (defined STM32L063xx) || \
+	(defined STM32L071xx) || (defined STM32L072xx) || (defined STM32L073xx) || \
+	(defined STM32L081xx) || (defined STM32L082xx) || (defined STM32L083xx)
+#include "stm32l0xx_hal.h"
+#elif defined (STM32L412xx) || defined (STM32L422xx) || \
+	defined (STM32L431xx) || (defined STM32L432xx) || defined (STM32L433xx) || defined (STM32L442xx) || defined (STM32L443xx) || \
+	defined (STM32L451xx) || defined (STM32L452xx) || defined (STM32L462xx) || \
+	defined (STM32L471xx) || defined (STM32L475xx) || defined (STM32L476xx) || defined (STM32L485xx) || defined (STM32L486xx) || \
+    defined (STM32L496xx) || defined (STM32L4A6xx) || \
+    defined (STM32L4R5xx) || defined (STM32L4R7xx) || defined (STM32L4R9xx) || defined (STM32L4S5xx) || defined (STM32L4S7xx) || defined (STM32L4S9xx)
+#include "stm32l4xx_hal.h"
+#else
+#error Platform not implemented
+#endif
+
+#ifndef RFM95_SPI_TIMEOUT
+#define RFM95_SPI_TIMEOUT 10
+#endif
+
+/**
+ * Structure defining a handle describing am RFM95(W) transceiver.
+ */
+typedef struct {
+
+	/**
+	 * The handle to the SPI bus for the device.
+	 */
+	SPI_HandleTypeDef *spi_handle;
+
+	/**
+	 * The port of the NSS pin.
+	 */
+	GPIO_TypeDef *nss_port;
+
+	/**
+	 * The NSS pin.
+	 */
+	uint16_t nss_pin;
+
+	/**
+	 * The port of the RST pin.
+	 */
+	GPIO_TypeDef *nrst_port;
+
+	/**
+	 * The RST pin.
+	 */
+	uint16_t nrst_pin;
+
+	/**
+	 * The port of the IRQ pin.
+	 */
+	GPIO_TypeDef *irq_port;
+
+	/**
+	 * The IRQ pin.
+	 */
+	uint16_t irq_pin;
+
+} rfm95_handle_t;
+
+bool rfm95_init(rfm95_handle_t *handle);
